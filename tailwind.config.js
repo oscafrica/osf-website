2d0f2f020cf78d497ed9dcce58c43377f6846a66
bolajiayodeji <sirbeejay1@gmail.com>
2019-11-24 08:08:35 +0100

    config tailwind color keys to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,19 @@
 
 module.exports = {
   theme: {
-    colors: {
-      transparent: 'transparent',
-      black: '#000',
-      white: '#fff',
-      gray: {
-        100: '#f7fafc',
+    extends: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        gray: {
+          100: '#f7fafc',
+          // ...
+          900: '#1a202c',
+        },
+  
         // ...
-        900: '#1a202c',
-      },
-
-      // ...
+      }
     }
   },
   variants: {},
