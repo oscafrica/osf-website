4ca880f92ff92e0265808ff4fb1f1b7ddc7d31f1
bolajiayodeji <sirbeejay1@gmail.com>
2019-11-24 07:24:34 +0100

    add section-components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,37 +1,44 @@
+import { graphql, useStaticQuery, Link } from "gatsby";
 import React from "react";
-
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
+import nextBillionCreators from "../images/NXTBC.png";
 
 function AboutPage() {
+  const { site } = useStaticQuery(graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`);
   return (
-    <Layout>
-      <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="About"
-      />
+    <div>
 
       <section className="flex flex-col md:flex-row items-center">
+
+      <figure className="w-2/3 md:w-1/3">
+          <img alt={site.siteMetadata.title}
+               src={nextBillionCreators}
+          />
+      </figure>
+
         <div className="md:w-2/3 md:mr-8">
           <blockquote className="border-l-4 border-gray-900 font-serif leading-loose pl-4 text-justify">
-            The point is... to live one's life in the full complexity of what
-            one is, which is something much darker, more contradictory, more of
-            a maelstrom of impulses and passions, of cruelty, ecstacy, and
-            madness, than is apparent to the civilized being who glides on the
-            surface and fits smoothly into the world.
+          Open Source Community Africa is a community aimed at creating and supporting the open source movement within Africa.
+          As a community, we intend to help integrate the act of open source contribution to African developers whilst strongly advocating
+          the movement of free and open source software. <br /><br />
+
+          Open Source Festival is a high profile event that would attract student delegates, developers, designers and corporate organizations
+          on a large scale with series of talks, workshops, and awareness of open-sourced developer tools.
+          The maiden edition is going to be a forum for networking, discussions and ideas proration around
+          latest happenings in technology as well as the growth of open source in Africa. <br /><br />
+
+          Through this festival, we intend to move Africans from just the billion users to the NEXT BILLION CREATORS.
           </blockquote>
-
-          <cite className="font-bold mt-4 text-right text-xs uppercase block">
-            – Thomas Nagel
-          </cite>
         </div>
-
-        <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={dogIllustration} />
-        </figure>
       </section>
-    </Layout>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,10 @@
-import { graphql, useStaticQuery, Link } from "gatsby";
 import React from "react";
 
+import AboutPage from "./about"
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import nextBillionCreators from "../images/NXTBC.png";
 
 function IndexPage() {
-  const { site } = useStaticQuery(graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`);
   return (
     <Layout>
       <SEO
@@ -22,14 +12,9 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="text-center">
-        <img
-          alt={site.siteMetadata.title}
-          className="block mx-auto w-1/2"
-          src={nextBillionCreators}
-        />
 
-      </section>
+      <AboutPage />
+
     </Layout>
   );
 }
