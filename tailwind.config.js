56122b09934089f223e8f0103ac31dd6d4c22728
bolajiayodeji <sirbeejay1@gmail.com>
2019-11-24 07:59:47 +0100

    add tailwaind color keys

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 // See https://tailwindcss.com/docs/configuration for details
 
 module.exports = {
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        // ...
+        900: '#1a202c',
+      },
+
+      // ...
+    }
+  },
   variants: {},
   plugins: []
 };
