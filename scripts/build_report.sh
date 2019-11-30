3bf9a20c081512e271290c3debcb27d9a51a41d3
NotJohs <notjohs@NotJohs.local>
2019-11-30 17:40:55 +0100

    chore: prefix linux instruction with shx

--- scripts/build_report.sh
+++ scripts/build_report.sh
@@ -9,15 +9,15 @@ NYC_DIR=./.nyc_output/
 echo "> build coverage report \n"
 
 
-[[ -f "$ROOT$REPORT" ]] || mkdir "$ROOT$REPORT"
+[[ -f "$ROOT$REPORT" ]] || shx mkdir "$ROOT$REPORT"
 
 [[ -f "$ROOT$CYPRESS$CFF" ]] &&
-    cp "$ROOT$CYPRESS$CFF" $ROOT$REPORT"report-cypress.json"
+    shx cp "$ROOT$CYPRESS$CFF" $ROOT$REPORT"report-cypress.json"
 
 [[ -f "$ROOT$JEST$CFF" ]] &&
-    cp "$ROOT$JEST$CFF" $ROOT$REPORT"report-jest.json"
+    shx cp "$ROOT$JEST$CFF" $ROOT$REPORT"report-jest.json"
 
-nyc merge $ROOT$REPORT $ROOT$REPORT$CF && mv $ROOT$REPORT$CF $NYC_DIR"out.json"
+nyc merge $ROOT$REPORT $ROOT$REPORT$CF && shx mv $ROOT$REPORT$CF $NYC_DIR"out.json"
 
 echo "\n> nyc report --reporter text-lcov --report-dir $ROOT"
 nyc report --reporter lcov --report-dir $ROOT
