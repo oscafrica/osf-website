b99d5b01676a7b939306cce04db54c1fbb627d9e
Favour Kelvin <favourkelvin17gmail.com>
2019-12-03 13:48:21 +0100

    complete radio btn implementation

---
+++ src/components/RadioButton.js/RadioButton.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';;
+import  RadioButton from '.';
+import styles from './style.module.css';
+
+export default {
+  title: 'RadioButton',
+  component: 'RadioButton'
+};
+const onChange = () => console.log('Value has changed');
+
+export const Radiobutton = () => 
+  <RadioButton
+  className={('Class Name', 'custom-class')}
+  onChange={('change')}
+  onBlur={('blur')}
+  name="my-radio-group"
+
+/>
+  
+  export const RadioButtonChecked = () => <RadioButton defaultChecked onChange={onChange}></RadioButton>
+  
+  export const DisabledRadioButton = () =>  <RadioButton disabled></RadioButton>
+  
+  export const DisabledCheckedRadioButton = () => <RadioButton disabled defaultChecked></RadioButton>
+  
+  
