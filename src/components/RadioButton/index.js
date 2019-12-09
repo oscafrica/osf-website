33352bbfb9cb5def2234178ce396ce0dc82cc0ea
Favour Kelvin <39309699+fakela@users.noreply.github.com>
2019-12-09 20:39:40 +0100

    fixed inconsistent spacing

--- src/components/RadioButton/index.js
+++ src/components/RadioButton/index.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import styles from './style.module.css';
 
 
-function RadioButton({value, onChange,label,style, onBlur, name, children, className,checked, ...props}) {
+function RadioButton({value, onChange, label, style, onBlur, name, children, className, checked, ...props}) {
     const _className = classNames(styles.oc_radiobutton_container, styles.oc_field, className);
     const _fieldClass = classNames(styles.oc_field_label, className)
     const _wrapper = classNames(styles.oc_wrapper, className)
@@ -19,7 +19,7 @@ function RadioButton({value, onChange,label,style, onBlur, name, children, class
                 onBlur={onBlur}
                 value={value}
                 checked={checked}
-                  {...props}
+                {...props}
             />
             {children}
     </label>
