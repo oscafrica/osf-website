2962b0f771b795d47ed700719dd8ad3a292589dd
Favour Kelvin <39309699+fakela@users.noreply.github.com>
2019-12-09 20:28:34 +0100

    removed oc_item from the index.js

--- src/components/RadioButton/index.js
+++ src/components/RadioButton/index.js
@@ -4,9 +4,9 @@ import styles from './style.module.css';
 
 
 function RadioButton({value, onChange,label,style, onBlur, name, children, className,checked, ...props}) {
-    const _className = classNames(styles.oc_radiobutton_container,styles.oc_field,className);
+    const _className = classNames(styles.oc_radiobutton_container, styles.oc_field, className);
     const _fieldClass = classNames(styles.oc_field_label, className)
-    const _wrapper = classNames(styles.oc_wrapper,styles.oc_item, className)
+    const _wrapper = classNames(styles.oc_wrapper, className)
   
     return (
         <div className={_wrapper}>
