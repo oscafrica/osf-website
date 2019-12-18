4ca0fc59e715272715f1d1591ba7b17b4005558d
Favour Kelvin <favourkelvin17gmail.com>
2019-12-18 18:46:15 +0100

    (feat) created checkbox component

---
+++ src/components/checkBox/checkBox.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CheckBox from ".";
+
+
+export default {
+  title: "CheckBox",
+  component: "CheckBox"
+};
+
+
+
+
+export const defaultCheckBox = () => <CheckBox/>;
+
+export const CheckBoxChecked = () => <CheckBox checked ></CheckBox>;
+
+export const disabledCheckBox = () => <CheckBox disabled style={{ backgroundColor: "#F0F0F0" }}></CheckBox>;
+
+export const checkedDisabledCheckBox = () => <CheckBox default checked disabled style={{ backgroundColor: "#F0F0F0" }} ></CheckBox>;
+
+
