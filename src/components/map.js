784b3f6c73a2d186a7caee62eafcf684b87f2148
bolajiayodeji <sirbeejay1@gmail.com>
2020-01-05 04:31:15 +0100

    fix(travel): fix venue location map

---
+++ src/components/map.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%"
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 6.5244,
+          lng: 3.3792
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+})(MapContainer);
--- src/pages/travel.js
+++ src/pages/travel.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo";
 
 import Nav from "../components/nav";
 import TravelHead from "../components/travelHead";
-import Map from "../components/venueMap";
+import VenueMap from "../components/map";
 import Footer from "../components/footer";
 
 function Schedule() {
@@ -14,7 +14,7 @@ function Schedule() {
       <SEO keywords={["Open Source Community Africa", "OSCA", "Open Source Festival", "OSF"]} title="Travel" />
       <Nav />
       <TravelHead />
-      <Map google="Zone Tech Park" />
+      <VenueMap />
       <Footer />
     </Layout>
   );
