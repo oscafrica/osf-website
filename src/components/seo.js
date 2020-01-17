e0a110e18520193b717174b35bc7fbf7dbd32b9b
bolajiayodeji <sirbeejay1@gmail.com>
2020-01-17 11:07:09 +0100

    fix(seo): update

--- src/components/seo.js
+++ src/components/seo.js
@@ -54,7 +54,7 @@ function SEO({ description, lang, meta, keywords, title }) {
           content: metaDescription
         },
         {
-          name: "image",
+          name: "og:image",
           content: site.siteMetadata.image
         },
         {
