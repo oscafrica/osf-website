5e00713a3ec645ac27c0ffb06ff1301b6a10bd8d
bolajiayodeji <sirbeejay1@gmail.com>
2020-01-26 23:29:26 +0100

    feat(nav): add close button to mobile nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -20,11 +20,24 @@ function Nav() {
           className="block lg:hidden flex items-center px-3 py-2 rounded text-white"
           onClick={() => toggleExpansion(!isExpanded)}
         >
-          <svg className="fill-current h-4 w-8" viewBox="0 0 25 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <title>Menu</title>
-            <line y1="1" x2="25" y2="1" stroke="white" strokeWidth="2" />
-            <path d="M6 10H25" stroke="white" strokeWidth="2" />
-          </svg>
+          {isExpanded ? (
+            <svg className="fill-current h-4 w-8" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <title>Close Menu</title>
+              <path
+                d="M11.0494 1.17733L1.65261 10.5744M1.65234 1.17733L11.0494 10.5744"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          ) : (
+            <svg className="fill-current h-4 w-8" viewBox="0 0 25 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <line y1="1" x2="25" y2="1" stroke="white" strokeWidth="2" />
+              <path d="M6 10H25" stroke="white" strokeWidth="2" />
+            </svg>
+          )}
         </button>
 
         <nav
