0b24a62eb6a93de838f19939275da640c604ee35
Josh Ike <joshike.no@gmail.com>
2020-02-13 20:17:29 +0100

    test: components/cta
        - should have 4 items
        - should have texts '800' & 'Attendees'

---
+++ src/components/__tests__/cta.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import Cta from "../cta";
+
+let wrapper;
+
+describe("cta component", () => {
+  beforeAll(() => {
+    wrapper = mount(<Cta />);
+  });
+
+  it("should have 4 items", () => {
+    expect(wrapper.find("section div"))
+      .to.have.exactly(4)
+      .descendants("div span");
+  });
+
+  it("should contain texts '800' and 'Attendees'", () => {
+    expect(wrapper.text()).to.contain("Attendees");
+    expect(wrapper.text()).to.contain("800");
+  });
+});
