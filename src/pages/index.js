06fe1ea60d55547f375c9b9d8251ea19797fc1f9
Scentjuls <chidynwanya11@gmail.com>
2021-12-20 23:02:10 +0000

    fix(index.js): made changes to the sponsorship deck document

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ function IndexPage() {
 
           <div className="flex flex-col lg:flex-row items-center my-10">
             <SponsorLink
-              href="/docs/OSCA-sponsorship-guide.pdf"
+              href="/docs/OSCA - Sponsorship Guide.pdf"
               className="px-10 py-4 bg-orange-primary rounded-full uppercase text-black text-sm border-black border-2 tracking-wide font-semibold"
               target="_blank"
               rel="noreferrer noopenner"
