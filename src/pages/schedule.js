a4118ce4c06cac6de3e60c71b52affac434d03bd
Bolaji <sirbeejay1@gmail.com>
2022-03-02 00:15:08 +0100

    feat: 🎸 add schedule page

---
+++ src/pages/schedule.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/SEO/seo";
+
+import Nav from "../components/2022/nav";
+import Breadcrumb from "../components/2022/breadcrumb";
+import Sched from "../components/schedEmbed";
+import Footer from "../components/2022/footer";
+
+function Schedule() {
+  return (
+    <Layout>
+      <SEO keywords={["Open Source Community Africa", "OSCA", "Open Source Festival", "OSF"]} title="Schedule" />
+      <Nav />
+      <Breadcrumb name="Event Schedule" />
+      <Sched slug="oscafest22" />
+      <Footer />
+    </Layout>
+  );
+}
+
+export default Schedule;
