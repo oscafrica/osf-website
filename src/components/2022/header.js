4bf1c15f4bdb5c191eec722dbf6e9b31a28aae2e
Scentjuls <chidynwanya11@gmail.com>
2022-03-08 20:35:08 +0000

    fix(header.js): header text
    Fixed the overlapping header text on the tab screen

--- src/components/2022/header.js
+++ src/components/2022/header.js
@@ -15,29 +15,29 @@ function Header() {
       </div>
       <div className="flex flex-col z-30 absolute left-0 w-full min-h-screen">
         <div className="z-20 flex flex-wrap mt-20 lg:mt-10 mb-36 lg:flex-row px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 pb-10 sm:pb-6 lg:pt-40 lg:pb-12 xl:pb-24 2xl:pt-56 2xl:pb-48 sm:text-left">
-          <div className="w-full xl:w-1/2 lg:w-1/2 sm:w-2/3 mb-4 relative z-10 text-white">
-            <h1 className="font-anisette text-3xl lg:text-5xl leading-tight font-bold uppercase">
+          <div className="w-full md:w-full xl:w-1/2 lg:w-1/2 sm:w-2/3 mb-4 relative z-10 text-white">
+            <h1 className="font-anisette text-3xl md:text-xl lg:text-5xl leading-tight font-bold uppercase">
               <div className="bg-orange-primary mb-2 table px-1 py-2">Open Source</div>
               <div className="bg-orange-primary mb-2 table px-1 py-2">Festival 2022</div>
               <div className="bg-orange-primary mb-2 table px-1 py-2"> + Sustain</div>
             </h1>
             <div className="font-anisette flex flex-wrap mt-2 justify-content-between">
-              <div className="text-lg lg:text-lg uppercase bg-dark-blue-600 table mr-5 p-1">MARCH 24 - 26, 2022</div>
-              <div className="lg:-ml-2 text-lg lg:text-lg uppercase bg-dark-blue-600 table p-1">
+              <div className="text-lg md:text-md lg:text-lg uppercase bg-dark-blue-600 table mr-5 p-1">MARCH 24 - 26, 2022</div>
+              <div className="lg:-ml-2 text-lg md:text-md lg:text-lg uppercase bg-dark-blue-600 table p-1">
                 Lagos, Nigeria | Hybrid
               </div>
             </div>
             <p className="font-anisette text-lg lg:text-lg uppercase bg-dark-blue-600 mt-2 table p-1"> #OSCAFEST2022</p>
-            <p className="text-md lg:text-md text-white-700 mt-4 table">
+            <p className="text-md md:text-sm lg:text-md text-white-700 mt-4 table">
               An annual open source gathering under Open <br />
               Source Community Africa
             </p>
-            <div className="flex font-bold mt-10 lg:mt-0">
+            <div className="flex font-bold mt-10 md:mt-0">
               <a
                 href="https://opencollective.com/osca/events/open-source-festival-2022-e08a15a2"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="font-anisette text-xs w-1/2 block sm:inline-block sm:w-auto lg:mt-8 md:mt-8 mr-5 px-12 py-6 bg-orange-primary text-black text-center uppercase"
+                className="font-anisette text-xs w-1/2 block sm:inline-block sm:w-auto lg:mt-8 md:mt-0 mr-5 px-12 py-6 bg-orange-primary text-black text-center uppercase"
               >
                 Register
               </a>
@@ -45,7 +45,7 @@ function Header() {
                 href="/2020"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="font-anisette text-xs w-1/2 block sm:inline-block sm:w-auto lg:mt-8 md:mt-8 px-12 py-6 bg-army-green-primary text-white text-center uppercase"
+                className="font-anisette text-xs w-1/2 block sm:inline-block sm:w-auto lg:mt-8 md:mt-0 px-12 py-6 bg-army-green-primary text-white text-center uppercase"
               >
                 View past event
               </a>
