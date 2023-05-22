f3ddded80a9c1491c1c0fb4c750cc9bcea97fac8
Josh <mail@chukwuanu.com>
2023-05-22 02:36:19 +0100

    feat: add mobile navbar

--- src/components/2023/nav.js
+++ src/components/2023/nav.js
@@ -1,21 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 
 import { homeBackground } from "./cloudImages";
 
+const routes = [
+  {
+    route: "/#about",
+    title: "About"
+  },
+  {
+    route: "/#speakers",
+    title: "Speakers"
+  },
+  // {
+  //   route: "/2023/schedule",
+  //   title: "Schedule"
+  // },
+  // {
+  //   route: "/2023/sustain-africa",
+  //   title: "Sustain Africa"
+  // },
+  // {
+  //   route: "/2023/travel",
+  //   title: "Travel"
+  // },
+  {
+    route: "https://www.flickr.com/photos/oscafrica/albums/with/72177720298698442",
+    title: "Gallery"
+  }
+]
+
 function Nav() {
   const [isExpanded, toggleExpansion] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    window.scrollY >= 120 ? setNavbar(true) : setNavbar(false);
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
 
   return (
-    <>
-      <div
-        className={"container inset-x-0 mx-auto fixed top-0 z-40 bg-white mt-10 sm:px-8 lg:px-16 xl:px-10 2xl:px-64"}
-      >
-        <div
-          className={`${
-            isExpanded ? "bg-army-green-primary" : "bg-transparent"
-          } flex flex-wrap items-center justify-between max-w-8xl mx-auto p-2 lg:py-5`}
-        >
+    <div className="relative">
+      <div className={`${
+          isExpanded ? "relative" : "fixed"
+        } container inset-x-0 mx-auto fixed top-0 z-40 bg-white mt-10 px-4 lg:px-16 xl:px-10 2xl:px-64 w-[90%]`}>
+        <div className="bg-white flex flex-wrap items-center justify-between max-w-8xl mx-auto p-2 lg:py-5 ">
           <Link className="z-20 flex items-center no-underline text-white" to="/">
             <img
               alt="Open Source Community Africa's Logo"
@@ -25,62 +58,40 @@ function Nav() {
           </Link>
 
           <button
-            className="block lg:hidden items-center px-3 py-2 rounded text-white"
-            onClick={() => toggleExpansion(!isExpanded)}
+            className="block lg:hidden items-center px-3 py-2 rounded text-black"
+            onClick={() => {window.scrollTo(0,0); toggleExpansion(!isExpanded)}}
           >
             {isExpanded ? (
-              <svg className="fill-current h-4 w-8" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <title>Close Menu</title>
-                <path
-                  d="M11.0494 1.17733L1.65261 10.5744M1.65234 1.17733L11.0494 10.5744"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+              <span className="fill-current h-4 w-8" >
+                <svg  width="15" height="55" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <title>Close Menu</title>
+                  <path
+                    d="M11.0494 1.17733L1.65261 10.5744M1.65234 1.17733L11.0494 10.5744"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </span>
             ) : (
-              <svg className="fill-current h-4 w-8" viewBox="0 0 25 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <title>Menu</title>
-                <line y1="1" x2="25" y2="1" stroke="white" strokeWidth="2" />
-                <path d="M6 10H25" stroke="white" strokeWidth="2" />
-              </svg>
+              <span className="fill-current h-4 w-8">
+                <svg width="44" height="55" viewBox="0 0 44 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="16" y="16.0547" width="27.9971" height="2" fill="#0B0B20"/>
+                  <rect x="16" y="26.0547" width="27.9971" height="2" fill="#0B0B20"/>
+                  <rect x="16" y="36.0547" width="27.9971" height="2" fill="#0B0B20"/>
+                </svg>
+              </span>
             )}
           </button>
 
           <nav
-            className={`${
-              isExpanded ? "block" : "hidden"
-            } flex flex-col py-24 min-h-screen min-w-full text-center lg:py-0 lg:block lg:flex-row lg:items-center lg:w-auto lg:min-h-0 lg:min-w-0 font-workSans`}
+            id="desktop-nav"
+            className="hidden lg:block flex flex-col py-24 bg-black text-white lg:text-black lg:bg-white min-h-screen min-w-full text-center lg:py-0 lg:block lg:flex-row lg:items-center lg:w-auto lg:min-h-0 lg:min-w-0"
           >
-            {[
-              {
-                route: "/#about",
-                title: "About"
-              },
-              {
-                route: "/#speakers",
-                title: "Speakers"
-              },
-              // {
-              //   route: "/2023/schedule",
-              //   title: "Schedule"
-              // },
-              // {
-              //   route: "/2023/sustain-africa",
-              //   title: "Sustain Africa"
-              // },
-              // {
-              //   route: "/2023/travel",
-              //   title: "Travel"
-              // },
-              {
-                route: "https://www.flickr.com/photos/oscafrica/albums/with/72177720298698442",
-                title: "Gallery"
-              }
-            ].map((link) => (
+            {routes.map((link) => (
               <a
-                className="font-workSans block mt-0 mb-auto no-underline text-black text-xl lg:inline-block lg:-my-2 lg:ml-8 lg:text-sm uppercase font-bold"
+                className="font-humane tracking-wider lg:font-clashDisplay block mt-0 mb-5 text-left no-underline text-6xl lg:inline-block lg:-my-2 lg:ml-8 lg:text-sm uppercase font-bold"
                 key={link.title}
                 href={link.route}
               >
@@ -90,7 +101,25 @@ function Nav() {
           </nav>
         </div>
       </div>
-    </>
+  
+      <nav
+        id="mobile-nav"
+        className={`${
+          isExpanded ? "block" : "hidden"
+        }  z-40 flex flex-col pt-10 px-10 bg-black text-white min-h-screen min-w-full text-center absolute`}
+      >
+        {routes.map((link) => (
+          <a
+            className="font-humane tracking-wider lg:font-clashDisplay block mt-0 mb-5 text-left no-underline text-6xl lg:inline-block lg:-my-2 lg:ml-8 lg:text-sm uppercase font-bold"
+            key={link.title}
+            href={link.route}
+            onClick={() => toggleExpansion(!isExpanded)}
+          >
+            {link.title}
+          </a>
+        ))}
+      </nav>
+    </div>
   );
 }
 
