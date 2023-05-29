7264c581e01535d3aed9a944a2795f9e00e95905
Josh <mail@chukwuanu.com>
2023-05-29 14:50:03 +0100

    feat: declare scrollbar css

--- src/components/2023/speakers.js
+++ src/components/2023/speakers.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/scrollbar";
+import "./scrollbar.css";
 
 import { homeBackground, speakers } from "./cloudImages";
 
