c63f2d9f72c068a9223f3290f46ef864ad2363f2
alabobriggs <alabobriggs@gmail.com>
2025-07-21 12:46:13 +0100

    update session styling

--- src/pages/speaker-schedule.js
+++ src/pages/speaker-schedule.js
@@ -10,20 +10,14 @@ function SpeakerSchedulePage() {
     <Layout>
       <SEO title="Speaker Schedule" />
       <Nav />
-      <div className="min-h-screen bg-gray-50 py-12">
-        <div className="container mx-auto px-4">
-          <div className="bg-white rounded-lg shadow-lg p-2">
-            <iframe
-              src="https://sessionize.com/view/iwyk9p10/GridSmart?format=Embed_Styled_Html&isDark=False&title=Open%20Source%20Festival%202025"
-              width="100%"
-              height="800"
-              frameBorder="0"
-              title="Open Source Festival 2025 Schedule"
-              style={{ border: "none" }}
-            ></iframe>
-          </div>
-        </div>
-      </div>
+      <iframe
+        src="https://sessionize.com/view/iwyk9p10/GridSmart?format=Embed_Styled_Html&isDark=False&title=Open%20Source%20Festival%202025"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        title="Open Source Festival 2025 Schedule"
+        className="border-none -mt-[130px] lg:-mt-[90px]"
+      ></iframe>
       <Footer />
     </Layout>
   );
