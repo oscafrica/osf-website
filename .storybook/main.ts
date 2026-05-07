faf1a779aba690af2a2ad0923a7a53c6b97c19eb
Bolaji Ayodeji <sirbeejay1@gmail.com>
2026-05-07 09:35:08 +0100

    fix: lint issyes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,8 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc"
-  ],
-  "framework": "@storybook/react-webpack5"
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-webpack5-compiler-swc"],
+  framework: "@storybook/react-webpack5"
 };
 export default config;
